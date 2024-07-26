2e2a7e8f5c2bfc5432bbad57bb3d783a47d665da
TaekyungHa <86tkstar@gmail.com>
2024-07-26 10:33:42 +0900

    add Tab layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,7 @@ import { Tabs } from "expo-router";
 import { useThemeContext } from "@/hooks/useThemeContext";
 import GradientBackground from "@/components/GradientBackground";
 import { Ionicons } from "@expo/vector-icons";
-import AddButton from "@/components/AddButton";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 function Layout() {
   const colors = useThemeContext((s) => s.colors());
@@ -10,11 +10,11 @@ function Layout() {
     <Tabs
       sceneContainerStyle={{ backgroundColor: colors.background }}
       screenOptions={{
-        headerStyle: { backgroundColor: colors.background },
+        headerStyle: { backgroundColor: colors.background, height: 130 },
         headerTintColor: colors.title,
         headerTitleAlign: "left",
         headerTitleStyle: {
-          fontSize: 24,
+          fontSize: 30,
           fontWeight: "bold",
         },
         tabBarBackground: () => (
@@ -30,7 +30,30 @@ function Layout() {
         name="index"
         options={{
           headerTitle: "Expenses Tracker",
-          headerRight: () => <AddButton title={"New"} />,
+          headerRight: () => (
+            <View
+              // style={{
+              //   backgroundColor: colors.btnBackground,
+              //   borderRadius: 10,
+              //   marginRight: 20,
+              //   paddingVertical: 10,
+              //   paddingHorizontal: 15,
+              //   shadowColor: colors.btnBackground,
+              //   // shadowOffset: {
+              //   //   width: 0,
+              //   //   height: 7,
+              //   // },
+              //   // shadowOpacity: 0.43,
+              //   // shadowRadius: 9.51,
+              //   elevation: 20,
+              // }}
+              style={[styles.card, styles.elevation]}
+            >
+              <TouchableOpacity>
+                <Text>Click</Text>
+              </TouchableOpacity>
+            </View>
+          ),
           tabBarIcon: ({ focused, size, color }) => (
             <Ionicons
               name={focused ? "home" : "home-outline"}
@@ -70,5 +93,26 @@ function Layout() {
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginBottom: 13,
+  },
+  card: {
+    backgroundColor: "#52006A",
+    borderRadius: 10,
+    marginRight: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    width: "100%",
+    marginVertical: 10,
+  },
+  elevation: {
+    elevation: 20,
+    shadowColor: "#52006A",
+  },
+});
 
 export default Layout;
